d623d41b180239ff29ff44bd1dfa57b1432d59cd
Victor Hugo <victorhugo.pessoal.guardiola@gmail.com>
2026-04-26 12:14:42 +0100

    módulo 05 Estado Global y React Router | Rutas protegidas con React Router y estado global

--- modulo-05-EstadoGlobal-y-ReactRouter/src/App.jsx
+++ modulo-05-EstadoGlobal-y-ReactRouter/src/App.jsx
@@ -6,12 +6,16 @@ import { LoadingSpinner } from "./components/LoadingSpinner.jsx";
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
 import { NetworkToastCard } from "./components/NetworkToastCard.jsx";
+import { ProtectedRoute } from "./components/ProtectedRoute.jsx";
 
 const NotFoundPage = lazy(() => import("./pages/404.jsx"));
 const HomePage = lazy(() => import("./pages/Home.jsx"));
 const SearchPage = lazy(() => import("./pages/Search.jsx"));
 const Contact = lazy(() => import("./pages/Contact.jsx"));
 const JobDetail = lazy(() => import("./pages/Detail.jsx"));
+const ProfilePage = lazy(() => import("./pages/ProfilePage.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
+const Register = lazy(() => import("./pages/Register.jsx"));
 
 function App() {
   const { isOnline, isShown, isMounted } = useNetworkStatus();
@@ -40,6 +44,16 @@ function App() {
           <Route path="/search" element={<SearchPage />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/jobs/:jobId" element={<JobDetail />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute redirectTo="/login">
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
