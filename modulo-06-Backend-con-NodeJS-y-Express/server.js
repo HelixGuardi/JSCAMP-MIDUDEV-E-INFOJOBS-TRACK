ec58a6f87c7a9892774de85776f6f1b9ced2c5a7
Victor Hugo <victorhugo.pessoal.guardiola@gmail.com>
2026-06-01 12:43:17 +0100

    modulo-06 Backend con Node.js y Express | clase: Filtrar usuarios con query params

--- modulo-06-Backend-con-NodeJS-y-Express/server.js
+++ modulo-06-Backend-con-NodeJS-y-Express/server.js
@@ -12,25 +12,65 @@ function sendJson(res, statusCode, data) {
 }
 
 const users = [
-  { id: 1, name: "Helix_Guardi" },
-  { id: 2, name: "Victor Hugo Guardiola" },
+  {
+    id: 1,
+    name: "Helix_Guardi",
+  },
+  {
+    id: 2,
+    name: "Victor Hugo Guardiola",
+  },
+  {
+    name: "MiduDev",
+    id: "01e23d0b-39c3-4aa8-91da-4ef5626a66bf",
+  },
+  {
+    name: "VayaMateo26",
+    id: "67d59c9b-2c9a-437c-b09a-e92abb2704d2",
+  },
+  {
+    name: "ElverGalarga",
+    id: "16835690-d29a-4d09-8cc7-4737f79d258b",
+  },
+  {
+    name: "pheralb",
+    id: "ff115f15-ca43-4761-8182-507df653ed8f",
+  },
 ];
 
 const server = createServer(async (req, res) => {
   const { method, url } = req;
 
+  const [pathname, queryString] = url.split("?");
+
+  const searchParams = new URLSearchParams(queryString);
+
   if (method === "GET") {
-    if (url === "/users") {
-      return sendJson(res, 200, users);
+    if (pathname === "/users") {
+      if (
+        Number.isNaN(Number(searchParams.get("limit") || 0)) ||
+        Number.isNaN(Number(searchParams.get("offset") || 0))
+      ) {
+        return sendJson(res, 400, {
+          error: "Limit and offset must be numbers",
+        });
+      }
+
+      const limit = Number(searchParams.get("limit")) || users.length;
+      const offset = Number(searchParams.get("offset")) || 0;
+
+      const paginatedUsers = users.slice(offset, offset + limit);
+
+      return sendJson(res, 200, paginatedUsers);
     }
 
-    if (url === "/health") {
+    if (pathname === "/health") {
       return sendJson(res, 200, { status: "ok", uptime: process.uptime() });
     }
   }
 
   if (method === "POST") {
-    if (url === "/users") {
+    if (pathname === "/users") {
       const body = await json(req);
 
       if (!body || !body.name) {
