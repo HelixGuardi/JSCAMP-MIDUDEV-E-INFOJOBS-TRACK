df91c8b9b70e513c3f7e437e965c84fbb3557c6d
Victor Hugo <victorhugo.pessoal.guardiola@gmail.com>
2026-06-01 12:27:47 +0100

    modulo-06 Backend con Node.js y Express | clase: Crear usuarios con POST en una API Node.js

--- modulo-06-Backend-con-NodeJS-y-Express/server.js
+++ modulo-06-Backend-con-NodeJS-y-Express/server.js
@@ -1,4 +1,6 @@
 import { createServer } from "node:http";
+import { randomUUID } from "node:crypto";
+import { json } from "node:stream/consumers";
 
 process.loadEnvFile();
 const port = process.env.PORT ?? 3000;
@@ -9,22 +11,41 @@ function sendJson(res, statusCode, data) {
   return res.end(JSON.stringify(data));
 }
 
-const server = createServer((req, res) => {
+const users = [
+  { id: 1, name: "Helix_Guardi" },
+  { id: 2, name: "Victor Hugo Guardiola" },
+];
+
+const server = createServer(async (req, res) => {
   const { method, url } = req;
 
-  if (method !== "GET") {
-    return sendJson(res, 405, { error: "Method Not Allowed" });
+  if (method === "GET") {
+    if (url === "/users") {
+      return sendJson(res, 200, users);
+    }
+
+    if (url === "/health") {
+      return sendJson(res, 200, { status: "ok", uptime: process.uptime() });
+    }
   }
 
-  if (url === "/users") {
-    return sendJson(res, 200, [
-      { id: 1, name: "Helix_Guardi" },
-      { id: 2, name: "Victor Hugo Guardiola" },
-    ]);
-  }
+  if (method === "POST") {
+    if (url === "/users") {
+      const body = await json(req);
 
-  if (url === "/health") {
-    return sendJson(res, 200, { status: "ok", uptime: process.uptime() });
+      if (!body || !body.name) {
+        return sendJson(res, 400, { error: "Name is required!" });
+      }
+
+      const newUser = {
+        name: body.name,
+        id: randomUUID(),
+      };
+
+      users.push(newUser);
+
+      return sendJson(res, 201, { message: "Usuario creado" });
+    }
   }
 
   return sendJson(res, 404, { message: "Not Found" });
